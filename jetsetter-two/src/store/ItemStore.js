1cc71dd333e71742ce062ac9977fe76f30523671
Chester Heng <chester.heng@gmail.com>
2020-09-12 13:38:16 +0800

    05. MobX-List Model MobX Store

--- jetsetter-two/src/store/ItemStore.js
+++ jetsetter-two/src/store/ItemStore.js
@@ -1,4 +1,48 @@
 import { observable, computed, action } from 'mobx';
 import Item from './ItemModel';
 
-export default class ItemStore {}
+export default class ItemStore {
+  @observable items = [];
+  @observable unpackedItemsFilter = '';
+  @observable packedItemsFilter = '';
+
+  @computed get packedItems() {
+    return this.items.filter(item => item.packed);
+  }
+
+  @computed get unpackedItems() {
+    return this.items.filter(item => item.unpacked);
+  }
+
+  @computed get filteredPackedList() {
+    return this.items.filter(item => 
+      item.value.includes(this.packedItemsFilter)
+    ); 
+  }
+
+  @computed get filteredUnpackedList() {
+    return this.items.filter(item => 
+      item.value.includes(this.unpackedItemsFilter)
+    ); 
+  }
+
+  @action.bound
+  addItem(value) {
+    this.items.push(new Item(value, this));
+  }
+
+  @action.bound
+  updatePackedItemsFilter(value) {
+    this.packedItemsFilter = value;
+  }
+
+  @action.bound
+  updateUnpackedItemsFilter(value) {
+    this.unpackedItemsFilter = value;
+  }
+
+  @action.bound
+  remove(itemToRemove) {
+    this.items.filter(item => item === itemToRemove);
+  }
+}
